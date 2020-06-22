b77448241d55de082cfcf34d0ad35d18bdc7cf9e
yuandaxia <847345650@qq.com>
2020-06-22 23:18:20 +0800

    会员Controller创建

---
+++ src/main/java/com/wowoniu/fendian/web/api/app/controller/member/MemberController.java
@@ -0,0 +1,17 @@
+package com.wowoniu.fendian.web.api.app.controller.member;
+
+import io.swagger.annotations.Api;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 会员管理控制层
+ *
+ * @author yuany
+ * @date 2020-06-22
+ */
+@Api(value = "会员管理控制层", tags = "会员统计管理设置接口")
+@RestController
+@RequestMapping("/member")
+public class MemberController {
+}
