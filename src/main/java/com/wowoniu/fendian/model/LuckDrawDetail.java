211088648b7be8a1b950a832ffab3c7dc6b5c2fc
yuandaxia <847345650@qq.com>
2020-07-05 11:56:59 +0800

    转盘/金蛋抽奖实体

---
+++ src/main/java/com/wowoniu/fendian/model/LuckDrawDetail.java
@@ -0,0 +1,200 @@
+package com.wowoniu.fendian.model;
+
+import io.swagger.annotations.ApiModelProperty;
+import org.joda.time.DateTime;
+
+import java.io.Serializable;
+
+/**
+ * @author yuany
+ * @date 2020-07-05
+ */
+public class LuckDrawDetail implements Serializable {
+
+    @ApiModelProperty("主键ID")
+    private String id;
+
+    @ApiModelProperty("抽奖ID")
+    private String luckDrawId;
+
+    @ApiModelProperty("奖项名称")
+    private String name;
+
+    @ApiModelProperty("奖品类型（0：优惠券；1：自定义奖品）")
+    private String type;
+
+    @ApiModelProperty("优惠券类型（0：代金券；1：折扣券）")
+    private String couponType;
+
+    @ApiModelProperty("优惠金额（分）")
+    private Integer preferential;
+
+    @ApiModelProperty("优惠券满多少可用（分；默认0）")
+    private Integer satisfy;
+
+    @ApiModelProperty("使用范围")
+    private String range;
+
+    @ApiModelProperty("有效期类型（0：置顶日期；1：有效天数）")
+    private String effectiveType;
+
+    @ApiModelProperty("有效天数")
+    private Integer effectiveDay;
+
+    @ApiModelProperty("奖品总数量")
+    private Integer prizeNumber;
+
+    @ApiModelProperty("每日奖品数量")
+    private Integer dayMax;
+
+    @ApiModelProperty("券有效开始时间")
+    private DateTime startTime;
+
+    @ApiModelProperty("券有效结束时间")
+    private DateTime endTime;
+
+    @ApiModelProperty("折扣（两位小数）")
+    private Double discount;
+
+    @ApiModelProperty("奖品名称")
+    private String prizeName;
+
+    @ApiModelProperty("奖品图片")
+    private String pictureUrl;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getLuckDrawId() {
+        return luckDrawId;
+    }
+
+    public void setLuckDrawId(String luckDrawId) {
+        this.luckDrawId = luckDrawId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getCouponType() {
+        return couponType;
+    }
+
+    public void setCouponType(String couponType) {
+        this.couponType = couponType;
+    }
+
+    public Integer getPreferential() {
+        return preferential;
+    }
+
+    public void setPreferential(Integer preferential) {
+        this.preferential = preferential;
+    }
+
+    public Integer getSatisfy() {
+        return satisfy;
+    }
+
+    public void setSatisfy(Integer satisfy) {
+        this.satisfy = satisfy;
+    }
+
+    public String getRange() {
+        return range;
+    }
+
+    public void setRange(String range) {
+        this.range = range;
+    }
+
+    public String getEffectiveType() {
+        return effectiveType;
+    }
+
+    public void setEffectiveType(String effectiveType) {
+        this.effectiveType = effectiveType;
+    }
+
+    public Integer getEffectiveDay() {
+        return effectiveDay;
+    }
+
+    public void setEffectiveDay(Integer effectiveDay) {
+        this.effectiveDay = effectiveDay;
+    }
+
+    public Integer getPrizeNumber() {
+        return prizeNumber;
+    }
+
+    public void setPrizeNumber(Integer prizeNumber) {
+        this.prizeNumber = prizeNumber;
+    }
+
+    public Integer getDayMax() {
+        return dayMax;
+    }
+
+    public void setDayMax(Integer dayMax) {
+        this.dayMax = dayMax;
+    }
+
+    public DateTime getStartTime() {
+        return startTime;
+    }
+
+    public void setStartTime(DateTime startTime) {
+        this.startTime = startTime;
+    }
+
+    public DateTime getEndTime() {
+        return endTime;
+    }
+
+    public void setEndTime(DateTime endTime) {
+        this.endTime = endTime;
+    }
+
+    public Double getDiscount() {
+        return discount;
+    }
+
+    public void setDiscount(Double discount) {
+        this.discount = discount;
+    }
+
+    public String getPrizeName() {
+        return prizeName;
+    }
+
+    public void setPrizeName(String prizeName) {
+        this.prizeName = prizeName;
+    }
+
+    public String getPictureUrl() {
+        return pictureUrl;
+    }
+
+    public void setPictureUrl(String pictureUrl) {
+        this.pictureUrl = pictureUrl;
+    }
+}
---
+++ src/main/java/com/wowoniu/fendian/model/LuckDrawSet.java
@@ -0,0 +1,192 @@
+package com.wowoniu.fendian.model;
+
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import io.swagger.annotations.ApiParam;
+import org.joda.time.DateTime;
+
+import java.io.Serializable;
+
+/**
+ * @author yuany
+ * @date 2020-07-05
+ */
+@ApiModel("抽奖活动设置")
+public class LuckDrawSet implements Serializable {
+
+    @ApiModelProperty("主键ID")
+    private String id;
+
+    @ApiModelProperty("商家ID")
+    private String userId;
+
+    @ApiModelProperty("标题")
+    private String title;
+
+    @ApiModelProperty("开始时间")
+    private DateTime startTime;
+
+    @ApiModelProperty("结束时间")
+    private DateTime endTime;
+
+    @ApiModelProperty("参加活动是否填写手机号（N：否；Y：是）")
+    private String phoneEnable;
+
+    @ApiModelProperty("单人每天最多抽奖次数")
+    private Integer personDay;
+
+    @ApiModelProperty("单人最多可中奖次数")
+    private Integer personFrequency;
+
+    @ApiModelProperty("每次抽奖中奖概率（整数+%）")
+    private Integer probability;
+
+    @ApiModelProperty("增加次数")
+    private Integer increaseTimes;
+
+    @ApiModelProperty("活动描述")
+    private String describle;
+
+    @ApiModelProperty("活动图片地址（最多9张‘，’分隔）")
+    private String pictureUrl;
+
+    @ApiModelProperty("活动规则")
+    private String rule;
+
+    @ApiModelProperty("防刷（N：禁用；Y：启用）")
+    private String preventBrush;
+
+    @ApiModelProperty("背景音乐ID")
+    private String musicId;
+
+    @ApiModelProperty("类型（0：转盘；1：砸金蛋）")
+    private String type;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public DateTime getStartTime() {
+        return startTime;
+    }
+
+    public void setStartTime(DateTime startTime) {
+        this.startTime = startTime;
+    }
+
+    public DateTime getEndTime() {
+        return endTime;
+    }
+
+    public void setEndTime(DateTime endTime) {
+        this.endTime = endTime;
+    }
+
+    public String getPhoneEnable() {
+        return phoneEnable;
+    }
+
+    public void setPhoneEnable(String phoneEnable) {
+        this.phoneEnable = phoneEnable;
+    }
+
+    public Integer getPersonDay() {
+        return personDay;
+    }
+
+    public void setPersonDay(Integer personDay) {
+        this.personDay = personDay;
+    }
+
+    public Integer getPersonFrequency() {
+        return personFrequency;
+    }
+
+    public void setPersonFrequency(Integer personFrequency) {
+        this.personFrequency = personFrequency;
+    }
+
+    public Integer getProbability() {
+        return probability;
+    }
+
+    public void setProbability(Integer probability) {
+        this.probability = probability;
+    }
+
+    public Integer getIncreaseTimes() {
+        return increaseTimes;
+    }
+
+    public void setIncreaseTimes(Integer increaseTimes) {
+        this.increaseTimes = increaseTimes;
+    }
+
+    public String getDescrible() {
+        return describle;
+    }
+
+    public void setDescrible(String describle) {
+        this.describle = describle;
+    }
+
+    public String getPictureUrl() {
+        return pictureUrl;
+    }
+
+    public void setPictureUrl(String pictureUrl) {
+        this.pictureUrl = pictureUrl;
+    }
+
+    public String getRule() {
+        return rule;
+    }
+
+    public void setRule(String rule) {
+        this.rule = rule;
+    }
+
+    public String getPreventBrush() {
+        return preventBrush;
+    }
+
+    public void setPreventBrush(String preventBrush) {
+        this.preventBrush = preventBrush;
+    }
+
+    public String getMusicId() {
+        return musicId;
+    }
+
+    public void setMusicId(String musicId) {
+        this.musicId = musicId;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+}
