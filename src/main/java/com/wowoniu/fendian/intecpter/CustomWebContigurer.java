b74122d6bba500f44228e7a4ad67cf0d9949efbf
yuanyang <yuanyang@zcnest.com>
2020-09-29 13:16:08 +0800

    更新

--- src/main/java/com/wowoniu/fendian/intecpter/CustomWebContigurer.java
+++ src/main/java/com/wowoniu/fendian/intecpter/CustomWebContigurer.java
@@ -23,7 +23,7 @@ public class CustomWebContigurer implements WebMvcConfigurer {
         registry.addInterceptor(loginIntercepter).addPathPatterns("/api/bank/*/**");
         registry.addInterceptor(loginIntercepter).addPathPatterns("/app/union/*/**");
         registry.addInterceptor(loginIntercepter).addPathPatterns("/activitySet/*/**");
-//        registry.addInterceptor(loginIntercepter).addPathPatterns("/member/*/**");
+        registry.addInterceptor(loginIntercepter).addPathPatterns("/member/*/**");
         registry.addInterceptor(loginIntercepter).addPathPatterns("/app/shop/*/**");
 //      registry.addInterceptor(loginIntercepter).addPathPatterns("/applet/*/**");
         //registry.addInterceptor(loginIntercepter).addPathPatterns("/api/us/login/getHomePage");
